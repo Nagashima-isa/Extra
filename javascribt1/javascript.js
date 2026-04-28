d91e0303a57b9f89c46a0ed38cd58545d37c7085
Nagashima-isa <isabella.nagashima@aluno.ifsp.edu.br>
2026-04-28 15:12:41 -0300

    Add files via upload

---
+++ javascribt1/javascript.js
@@ -0,0 +1,21 @@
+ <script>
+        function alterarTexto() {
+            document.getElementById('titulo').innerText = 'Texto Alterado!';
+        }
+
+        function alterarTexto() {
+            document.getElementById('titulo').innerText = 'Texto Alterado!';
+        }
+
+        function alterarTexto() {
+            document.getElementById('titulo').innerText = 'Texto Alterado!';
+        }
+    
+        function alterarTexto() {
+            document.getElementById('titulo').innerText = 'Texto Alterado!';
+        }
+   
+        function alterarTexto() {
+            document.getElementById('titulo').innerText = 'Texto Alterado!';
+        }
+    </script>
